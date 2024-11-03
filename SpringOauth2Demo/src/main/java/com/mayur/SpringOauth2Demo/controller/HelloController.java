7c393eeb1915d1d17eb0d584f998e679e637422f
Mayur <mayurdehade0504@gmail.com>
2024-11-03 16:51:21 +0530

    ouath2

---
+++ SpringOauth2Demo/src/main/java/com/mayur/SpringOauth2Demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.mayur.SpringOauth2Demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public  String greet() {
+        return "Hello... Welcome";
+    }
+}
